daa5600b244545b322c2f9d6e5d5e16c9ffe131a
Tarsio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-04-11 23:35:34 -0300

    working on Dockerfile, almost ready. I cannot connect to x11 yet

---
+++ lesson09/readme.md
@@ -0,0 +1,3 @@
+Download:
+
+http://beru.univ-brest.fr/~singhoff/cheddar/Cheddar-3.1-linux32-bin.tar.gz
---
+++ lesson11/run.sh
@@ -0,0 +1,2 @@
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix \
+		-e DISPLAY=unix$DISPLAY tarsioonofrio/cheddar
